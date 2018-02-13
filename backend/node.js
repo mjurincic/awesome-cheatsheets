9a4e8f54c404ed83ec79ed5fd4624ad1fa875782
Mauro <mauro@jurincic.net>
2018-02-13 11:05:56 +0100

    Update node.js

--- backend/node.js
+++ backend/node.js
@@ -636,3 +636,22 @@ const config = {
 
 module.exports = config;
 
+//Graceful shutdown in node.js
+var http = require('http');
+
+var server = http.createServer(function (req, res) {
+  setTimeout(function () { //simulate a long request
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+  }, 4000);
+}).listen(9090, function (err) {
+  console.log('listening http://localhost:9090/');
+  console.log('pid is ' + process.pid);
+});
+
+process.on('SIGTERM', function () {
+  server.close(function () {
+    process.exit(0);
+  });
+});
+
